7a6aea8ff18d956cf5a11b51738d8010e4e537c7
TimothyBelekollie <belekollietimothy2@gmail.com>
2024-01-23 16:10:19 +0200

    finish read and update part of crud for expense

--- expenseswebsite/expenses/urls.py
+++ expenseswebsite/expenses/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
    path('',views.index, name='expenses.index'),
    path('add-expense',views.add_expense,name='expenses.add'),
+   path('edit-expense/<int:pk>',views.edit_expense,name='expenses.edit'),
 ]
--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Category,Expense
 from django.contrib import messages
@@ -38,7 +38,42 @@ def add_expense(request):
         Expense.objects.create(owner=request.user,amount=amount,date=date,category=category,description=description)
         messages.success(request, 'Expense save successfully')
         return redirect('expenses.index')
+    
+    
+def edit_expense(request,pk):
+    #expense=Expense.objects.get(pk=pk)  ---This could also get the job done for me.
+    expense=get_object_or_404(Expense,pk=pk)
+    categories=Category.objects.all()
+    if request.method=="GET":
+        context={'expense':expense, 'values':expense,'categories':categories}
+      
+        return render(request, 'expenses/edit_expense.html',context)
+    
+    if request.method=="POST":
+            amount=request.POST['amount']
+            if not amount:
+                messages.error(request, 'Amount is required ')
+                return render(request,'expenses/edit_expense.html',context)
         
+            description=request.POST['description']
+            date=request.POST['date']
+            category=request.POST['category']
+            
+            
+            if not description:
+                messages.error(request, 'Description is required ')
+                return render(request,'expenses/edit_expense.html',context)
+            
+            
+            expense.owner=request.user
+            expense.amount=amount
+            expense.date=date
+            expense.category=category
+            expense.description=description
+            expense.save()
+            
+            messages.success(request, 'Expense updated successfully')
+            return redirect('expenses.index')
         
     
         
