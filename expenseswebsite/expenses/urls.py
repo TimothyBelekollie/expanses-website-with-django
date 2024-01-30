e4efd3d0d73a8b3db77ab55ae08615a07a13d515
TimothyBelekollie <belekollietimothy2@gmail.com>
2024-01-30 18:12:43 +0200

    expense search

--- expenseswebsite/expenses/urls.py
+++ expenseswebsite/expenses/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path,include
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from . import views
 urlpatterns = [
    path('',views.index, name='expenses.index'),
    path('add-expense',views.add_expense,name='expenses.add'),
    path('edit-expense/<int:pk>',views.edit_expense,name='expenses.edit'),
    path('destroy-expense/<int:pk>',views.destroy_expense,name='expenses.delete'),
+   
+   path('search-expenses',csrf_exempt(views.search_expenses), name='search_expenses'),
 ]
